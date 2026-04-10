555e7f9bfb364865bb53441e5d734f0dc6349ea7
FiveOnlineSite <helpdesk@fiveonline.in>
2026-04-10 16:13:09 +0530

    Update contactResponseRoute.js

--- routes/contact/contactResponseRoute.js
+++ routes/contact/contactResponseRoute.js
@@ -84,7 +84,7 @@ route.post("/", async (req, res) => {
     }
 
     return res.status(500).json({
-      message: "Failed to save contact or send email",
+      message: "Failed to save contact or send email testing",
       error: err.message,
     });
   }
